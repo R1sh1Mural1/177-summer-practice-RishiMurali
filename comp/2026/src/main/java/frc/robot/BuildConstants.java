38966ee22d0384fab77e1df1fee59c1501bb9cbd
R1sh1Mural1 <147774573+R1sh1Mural1@users.noreply.github.com>
2026-08-26 18:11:18 -0400

    Updated robot container to match hopper updates

--- comp/2026/src/main/java/frc/robot/BuildConstants.java
+++ comp/2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 328;
-  public static final String GIT_SHA = "66cc21f4815dd6667c4a53d9d4f7ef97205ce908";
-  public static final String GIT_DATE = "2026-07-22 20:33:10 EDT";
+  public static final int GIT_REVISION = 329;
+  public static final String GIT_SHA = "2591c1a331645b3691310787fc6fbe091a7b83de";
+  public static final String GIT_DATE = "2026-08-26 18:08:53 EDT";
   public static final String GIT_BRANCH = "testing";
-  public static final String BUILD_DATE = "2026-08-26 17:45:10 EDT";
-  public static final long BUILD_UNIX_TIME = 1787780710593L;
+  public static final String BUILD_DATE = "2026-08-26 18:10:37 EDT";
+  public static final long BUILD_UNIX_TIME = 1787782237624L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
